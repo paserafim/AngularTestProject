ea30031deca6475c1e26bd63b9fc3fe9bbe62f8c
Paulo Serafim <paserafim@gmail.com>
2018-10-26 11:21:06 +0100

    components updata class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'AngularDanWhalinCourseProject';
+export class AppComponent  implements OnInit {
+    title: string;
+  constructor() {}
+
+  ngOnInit() {
+    // we call the service that gets us the data
+    this.title = 'Hello World';
+  }
 }
+
